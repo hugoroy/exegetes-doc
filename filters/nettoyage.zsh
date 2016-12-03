c49f82526a3ddcd29f52d082e0661dca5568626d
Hugo Roy <hugo@hugoroy.eu>
2016-12-03 13:58:00 +0100

    espaces de guillemets simplifiés et prend en compte si parenthèse

--- filters/nettoyage.zsh
+++ filters/nettoyage.zsh
@@ -5,11 +5,8 @@ sed 's/<</«/g' | sed 's/>>/»/g' | \
 #sed 's/<<~/« /g' | sed 's/~>>/ »/g' | \
 #sed 's/«~/« /g' | sed 's/~»/ »/g' | \
     sed 's#"t":"Str","c":"«"},{"t":"Space","c":\[\]#"t":"Str","c":"« "#g' | \
-    sed 's#"t":"Space","c":\[\]},{"t":"Str","c":"»"#"t":"Str","c":" »"#g' | \
-    sed 's#"t":"Space","c":\[\]},{"t":"Str","c":"»\."#"t":"Str","c":" »."#g' | \
-    sed 's#"t":"Space","c":\[\]},{"t":"Str","c":"»,"#"t":"Str","c":" »,"#g' | \
-    sed 's#"t":"Space","c":\[\]},{"t":"Str","c":"»;"#"t":"Str","c":" »;"#g' | \
-    sed 's#"t":"Space","c":\[\]},{"t":"Str","c":"»?"#"t":"Str","c":" »?"#g' | \
-    sed 's#"t":"Space","c":\[\]},{"t":"Str","c":"»!"#"t":"Str","c":" »!"#g' | \
+    sed 's#"t":"Str","c":"(«"},{"t":"Space","c":\[\]#"t":"Str","c":"(« "#g' | \
+    sed 's#"t":"Space","c":\[\]},{"t":"Str","c":"»#"t":"Str","c":" »#g' | \
     sed 's#"t":"Str","c":"«"},{"t":"Space"#"t":"Str","c":"« "#g' | \
-    sed 's#"t":"Space"},{"t":"Str","c":"»"#"t":"Str","c":" »"#g'
+    sed 's#"t":"Str","c":"(«"},{"t":"Space"#"t":"Str","c":"(« "#g' | \
+    sed 's#"t":"Space"},{"t":"Str","c":"»#"t":"Str","c":" »#g'
