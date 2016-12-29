9b99d4ee213224c0d39891964340c680934fa2c8
Hugo Roy <hugo@hugoroy.eu>
2016-12-30 00:49:07 +0100

    mettre X pour les numéros de productions en docx

--- filters/docx.zsh
+++ filters/docx.zsh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sed 's#"t":"RawInline","c":\["html","<Llap>"\]#"t":"Str","c":"« "#g'  | \
-    sed 's#"t":"RawInline","c":\["html","<piece>"\]#"t":"Str","c":"(Prod. n° "#g'  | \
+    sed 's#"t":"RawInline","c":\["html","<piece>"\]#"t":"Str","c":"(Prod. n° XX-"#g'  | \
     sed 's#"t":"RawInline","c":\["html","<\/piece>"\]#"t":"Str","c":")"#g' 
     
 
