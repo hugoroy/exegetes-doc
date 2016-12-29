a5530e09678cd6ec1ece8c6d4e2c069da0cdcd39
Hugo Roy <hugo@hugoroy.eu>
2016-12-29 23:00:26 +0100

    renvoi aux productions commande latex

--- filters/latex.zsh
+++ filters/latex.zsh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-sed 's#"html","<Llap>"#"latex","\\\\Llap\{«\ \}"#g'
+sed 's#"html","<Llap>"#"latex","\\\\Llap\{«\ \}"#g'| \
+    sed 's#"html","<piece>"#"latex","\\\\piece\{"#g' | \
+    sed 's#"html","<\/piece>"#"latex","\}"#g' 
