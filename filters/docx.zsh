634716b61262e3ad58d70763a22e8cec62ab5237
Hugo Roy <hugo@hugoroy.eu>
2016-12-29 23:00:02 +0100

    références aux productions

--- filters/docx.zsh
+++ filters/docx.zsh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-sed 's#"t":"RawInline","c":\["html","<Llap>"\]#"t":"Str","c":"« "#g' 
+sed 's#"t":"RawInline","c":\["html","<Llap>"\]#"t":"Str","c":"« "#g'  | \
+    sed 's#"t":"RawInline","c":\["html","<piece>"\]#"t":"Str","c":"(Prod. n° "#g'  | \
+    sed 's#"t":"RawInline","c":\["html","<\/piece>"\]#"t":"Str","c":")"#g' 
+    
 
 
 
