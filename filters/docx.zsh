2afbf49112ebe006c6c0830511b828bf386760f0
Hugo Roy <hugo@hugoroy.eu>
2016-12-30 01:22:28 +0100

    ajout fonctionnalité bigskip en docx et latex

--- filters/docx.zsh
+++ filters/docx.zsh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
 sed 's#"t":"RawInline","c":\["html","<Llap>"\]#"t":"Str","c":"« "#g'  | \
+    sed 's#"t":"RawInline","c":\["html","<bigskip>"\]#"t":"Para","c":{"t":"Space"}#g' | \
     sed 's#"t":"RawInline","c":\["html","<piece>"\]#"t":"Str","c":"(Prod. n° XX-"#g'  | \
-    sed 's#"t":"RawInline","c":\["html","<\/piece>"\]#"t":"Str","c":")"#g' 
-    
-
-
-
-
-
+    sed 's#"t":"RawInline","c":\["html","<\/piece>"\]#"t":"Str","c":")"#g'
--- filters/latex.zsh
+++ filters/latex.zsh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 sed 's#"html","<Llap>"#"latex","\\\\Llap\{«\ \}"#g'| \
+    sed 's#"html","<bigskip>"#"latex","\\\\bigskip"#g'| \
     sed 's#"html","<piece>"#"latex","\\\\piece\{"#g' | \
     sed 's#"html","<\/piece>"#"latex","\}"#g' 
